a203cc68e58fcaa788757ed3cb1788f3b3366725
Gunti Pavan Durga Kumar <92723079+pavandurgakumar37@users.noreply.github.com>
2022-10-24 23:19:20 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import resumepic from "../../Assets/Projects/resumepic.png";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink ="https://drive.google.com/file/d/1R8mXNu5vwTrfPl86k1zR8dJGHbUC4Xkb/view?usp=sharing";
+const resumeLink = Process.env.myres;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
