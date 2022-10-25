78e57a87e01afde5ffdb255c2830026ee7b26fba
Gunti Pavan Durga Kumar <92723079+pavandurgakumar37@users.noreply.github.com>
2022-10-26 05:26:32 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -5,11 +5,10 @@ import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/pdk_new_resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
-import resumepic from "../../Assets/Projects/resumepic.png";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink = "https://drive.google.com/file/d/1R8mXNu5vwTrfPl86k1zR8dJGHbUC4Xkb/view?usp=sharing";
+const resumeLink ="https://drive.google.com/file/d/1R8mXNu5vwTrfPl86k1zR8dJGHbUC4Xkb/view?usp=sharing";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -25,23 +24,25 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
+            href={resumeLink}
+            target="_blank"
+            style={{ maxWidth: "250px", margin:"20px"}}
+          >
+            <AiOutlineDownload />
+            &nbsp;View Resume
+          </Button>
+        </Row>        
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
             href={pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download Resume
           </Button>
-        </Row>
-
-        <div>
-        <img
-                src={resumepic}
-                style={{ maxHeight: "1000px", margin: "50px", maxwidth:"450px" }}
-              />
-        </div>
-
-        
+        </Row>        
       </Container>
     </div>
   );
